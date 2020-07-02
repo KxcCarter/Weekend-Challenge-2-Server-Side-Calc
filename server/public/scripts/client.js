9c15a2b0c5c6044621d5125caade85e22e9b8b67
Kenneth Carter <kxccarter@gmail.com>
2020-07-02 16:39:30 -0500

    wrote calculate function -  creates a new object with math answer -  pushes to history array.

--- server/public/scripts/client.js
+++ server/public/scripts/client.js
@@ -3,3 +3,11 @@ $(document).ready(init);
 function init() {
     console.log('js and jQuery loaded');
 }
+
+// mathObject structure
+// {
+//     num1: Number,
+//     num2: Number,
+//     opperator: String,
+//     result: Number
+// }
--- server/server.js
+++ server/server.js
@@ -9,6 +9,62 @@ app.use(bodyParser.json());
 
 app.use(express.static('server/public'));
 
+const history = [];
+let mathSolved = null;
+
+// LOGIC //
+
+// calculates solution and pushes a new object containing solution into history array.
+function calculate(mathObject) {
+    let result = null;
+
+    let a,
+        b,
+        opp = [mathObject.num1, mathObject.num2, mathObject.opperator];
+
+    switch (opp) {
+        case opp === '+':
+            result = Number(a) + Number(b);
+            break;
+        case opp === '-':
+            result = Number(a) - Number(b);
+            break;
+        case opp === '*':
+            result = Number(a) * Number(b);
+            break;
+        case opp === '/':
+            result = Number(a) / Number(b);
+            break;
+    }
+    mathObject.result = result;
+    mathSolved = mathObject;
+    history.push(mathSolved);
+} // end calculate
+
+// GET AND POST ROUTES //
+
+// mathObject structure
+// {
+//     num1: Number,
+//     num2: Number,
+//     opperator: String,
+//     result: Number
+// }
+
+app.post('/calc', (req, res) => {
+    let mathObject = req.body;
+    calculate(mathObject);
+    res.sendStatus(201);
+});
+
+app.get('/solved', (req, res) => {
+    res.send(mathSolved);
+});
+
+app.get('/prevCalculations', (req, res) => {
+    res.send(history);
+});
+
 app.listen(PORT, () => {
     console.log('Server running on Port 5000');
 });
